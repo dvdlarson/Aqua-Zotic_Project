f9bbbf65fec897d669397b1eaddf2930a0843297
SallyTemple <pfende@gmail.com>
2018-07-11 23:13:23 -0700

    added header and footer to all pages

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './header.css';
 
 
-const Header = () => (
-  <div className='header-banner'>
-    <p>Aqua-zotic</p>
-  </div>
+const Header = props => (
+  <header className="header-banner">
+    <h1>Aqua-Zotic</h1>
+    <h2>
+      The Most Premier Exotic Freshwater Aquarium Fish Store In Arizona!
+    </h2>
+  </header>
 );
 
 export default Header;
