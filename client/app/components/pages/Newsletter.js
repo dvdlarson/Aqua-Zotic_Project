88ae74673c3674b71a356aeb6adbef23ce1c4a6b
SallyTemple <pfende@gmail.com>
2018-07-12 01:14:58 -0700

    added chatbot.

--- client/app/components/pages/Newsletter.js
+++ client/app/components/pages/Newsletter.js
@@ -2,7 +2,34 @@ import React from "react";
 
 const Newsletter = () => (
       <div>
-            <p>Newsletter</p>
+            <h1>Newsletter</h1>
+            <p>All about the fish and how awesome they are.</p>
+
+            <p>Then we talk about fish we know that fish live in the salted water which is located mostly in the oceans on the planet.
+            I believe that the fish might live for a while because of how they get their food to their babies that they produce.
+            There are so many different types of fish that live in the ocean and some of the fish can be deadly to humans and
+            some of them can be nice to humans for eample: small fish and dolphins and they can swim right next to you when you
+            are in the water. The deadliest fish the live in the ocean is sharks and whales. sharks can be deadly around humans
+            because they have sharp teeth that can bite a whole arm off someones body with speed. When someone's blood hits the
+            ocean water then the shark can smell it from miles way and that is pretty crazy. When the blood goes into the shark's
+            nose then the eyes turns black and the person should get out of the water quickly before the shark gets into the
+            area. The whale is different from other fish including sharks because of their size. The whales are the biggest
+            creatures in the world because they can group up to 80 to 100 feet in length and weight between 70 to 100 tons. The
+            food that whales eat can be anything from fish to squid to human. The whale can swallow anything in its path, it can
+            eat a human in one bite. There are different types of whales and they are: a blue whale, humback whale, and a grey
+            whale. There are different types of sharks and they are: The great white shark, Hammerhead Shark, tiger shark. The
+            great white sharks are the king of sharks because they are really deadly. The small fish are usually found at a store
+            or at the aquraim. Another deadly fish that lives in the ocean is called a squid. Sometimes there are jellyfish that
+            live in the ocean they can be deadly as well because of their stingers. Usually the jellyfish can grow up to 120
+            feet in length which is really long by my standards.</p>
+
+            <p>I believe that there are so many different types of species in the ocean and that is the true most of those species
+            can survive in really bad weather cases. There are some fish that live near the bottom of the ocean where it is really
+            dark that you can not see anything infront of your face. But there is one fish that you need to be carefull with and
+            that is the one that has a bulb that is hanging from the head of the fish. The teeth are so big that they can cut
+            through your skin as if it was a knife.</p>
+
+
       </div>
 
 );
--- client/app/index.js
+++ client/app/index.js
@@ -23,8 +23,7 @@ render((
   <Router>
     <div>
     <App>
-     
-    
+
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/counters" component={Counters}/>
@@ -35,7 +34,7 @@ render((
         <Route path="/login" component={Login} />
         <Route component={NotFound}/>
       </Switch>
-   
+      <iframe src="https://www.botlibre.com/chat?&id=22771587&embedded=true&chatLog=true&facebookLogin=false&application=918375383508331366&bubble=true&menubar=true&chooseLanguage=true&sendImage=true&background=%23fff&prompt=You+say&send=&css=https://www.botlibre.com/css/chatlog.css&language=en" width="500" height="400" frameborder="0" scrolling="auto"></iframe>
     </App>
     </div>
   </Router>
