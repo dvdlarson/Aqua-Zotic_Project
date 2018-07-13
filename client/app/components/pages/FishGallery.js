c6e1fa8db59ed618d8a7213adc98fda24ac44aaf
SallyTemple <pfende@gmail.com>
2018-07-12 23:01:55 -0700

    fish gallery styled

--- client/app/components/pages/FishGallery.js
+++ client/app/components/pages/FishGallery.js
@@ -2,22 +2,24 @@ import React from "react";
 
 const FishGallery = () => (
       <div>
+            <view>
             <h1>Fish Tanks of Satisfied Customers</h1>
+            <br/>
+            </view>
            
-            <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDdqyfyH526Chjqv6r-UkYjNXPlZ7CoJPzRqqi9Wm4-4jAvEvJGg" width="500" height="400" alt="Internet"/></div>
-            <div><img src="https://static.boredpanda.com/blog/wp-content/uuuploads/creative-aquariums/creative-aquariums-16-1.jpg " width="500" height="400" alt="Internet"/></div>
-            <div><img src="http://www.leszoosdanslemonde.com/zoos/asie/chine/shanghai_changfeng/enclosures/shanghai_changfeng_voiture_2007_2.jpg " width="500" height="400" alt="Internet"/></div>
-            <div><img src="https://i.kinja-img.com/gawker-media/image/upload/s--91937f6X--/c_scale,f_auto,fl_progressive,q_80,w_800/17o5cil6z9y7sjpg.jpg" width="500" height="400" alt="Internet"/></div>
-            <div><img src="http://luxurybusla.com/wp-content/uploads/2016/01/Opulent-Bedroom-with-Wooden-Bed-also-Soft-Mattres-plus-Wall-Lamps-also-Cool-Aquariums-with-Fish-also-Corrals-plus-Affordable-Lighting-Fixtures.jpg " width="500" height="400" alt="Internet"/></div>
-            <div><img src="https://i.pinimg.com/originals/67/a3/61/67a361b96290191779b9e5fa98ea0c60.jpg" width="500" height="400" alt="Internet"/></div>
-            <div><img src="https://s-media-cache-ak0.pinimg.com/originals/ed/cc/39/edcc392e56804a00672379f162720d29.jpg " width="500" height="400" alt="Internet"/></div>
-            <div><img src="https://forums.azbilliards.com/attachment.php?attachmentid=240074&d=1347249180 " width="500" height="400" alt="Internet"/></div>
-            <div><img src="http://scienceblogs.com/zooillogix/wp-content/blogs.dir/253/files/2012/04/i-e932ab2bea340bf97b5582d2546606cb-toilet%20fish%20tank.jpg " width="500" height="400" alt="Internet"/></div>
-            <div><img src=" http://www.corbrets.ca/wp-content/uploads/2016/10/pPETNA-5174966_main_enh.jpg" width="500" height="400" alt="Internet"/></div>
-            <div><img src="http://newportconsgroup.com/wp-content/uploads/2018/04/fishs-big-and-rich-touring-ohio-rescue-youtube-incredible-photo-design.jpg " width="500" height="400" alt="Internet"/></div>
-            <div><img src="https://fishinghumor.net/wp-content/uploads/2016/06/H9CT624B728LI8BA.jpeg " width="500" height="400" alt="Internet"/></div>
-            <div><img src="http://s7d1.scene7.com/is/image/PETCO/2299915-left-1 " width="500" height="400" alt="Internet"/></div>
-            <div><img src="http://newportconsgroup.com/wp-content/uploads/2018/05/imposing-gallon-fish-tank-images-inspirations-aqua-culture-led-aquarium-hood-for-aquariums-walmart.jpeg " width="500" height="400" alt="Internet"/></div>
+            <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDdqyfyH526Chjqv6r-UkYjNXPlZ7CoJPzRqqi9Wm4-4jAvEvJGg" className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src="https://static.boredpanda.com/blog/wp-content/uuuploads/creative-aquariums/creative-aquariums-16-1.jpg " className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src="http://www.leszoosdanslemonde.com/zoos/asie/chine/shanghai_changfeng/enclosures/shanghai_changfeng_voiture_2007_2.jpg " className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src="http://s7d1.scene7.com/is/image/PETCO/2299915-left-1" className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src=" http://newportconsgroup.com/wp-content/uploads/2018/05/imposing-gallon-fish-tank-images-inspirations-aqua-culture-led-aquarium-hood-for-aquariums-walmart.jpeg" className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src="https://i.pinimg.com/originals/67/a3/61/67a361b96290191779b9e5fa98ea0c60.jpg"  className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src="https://s-media-cache-ak0.pinimg.com/originals/ed/cc/39/edcc392e56804a00672379f162720d29.jpg " className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src=" http://newportconsgroup.com/wp-content/uploads/2018/05/fish-tank-sensationalitres-images-inspirations-interpet-aquaverse-glass-aquarium-premium-kit.jpg" className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src="http://scienceblogs.com/zooillogix/wp-content/blogs.dir/253/files/2012/04/i-e932ab2bea340bf97b5582d2546606cb-toilet%20fish%20tank.jpg " className="float-left" width="400" height="300" alt="Internet"/></div>
+            <div><img src=" http://www.corbrets.ca/wp-content/uploads/2016/10/pPETNA-5174966_main_enh.jpg" width="400" height="300" alt="Internet"/></div>
+          
+            
+           
       </div>
 
 );
--- client/app/index.js
+++ client/app/index.js
@@ -34,7 +34,7 @@ render((
         <Route path="/login" component={Login} />
         <Route component={NotFound}/>
       </Switch>
-      <iframe src="https://www.botlibre.com/chat?&id=22771587&embedded=true&chatLog=true&facebookLogin=false&application=918375383508331366&bubble=true&menubar=true&chooseLanguage=true&sendImage=true&background=%23fff&prompt=You+say&send=&css=https://www.botlibre.com/css/chatlog.css&language=en" width="500" height="400" frameborder="0" scrolling="auto"></iframe>
+      {/* <iframe src="https://www.botlibre.com/chat?&id=22771587&embedded=true&chatLog=true&facebookLogin=false&application=918375383508331366&bubble=true&menubar=true&chooseLanguage=true&sendImage=true&background=%23fff&prompt=You+say&send=&css=https://www.botlibre.com/css/chatlog.css&language=en" width="500" height="400" frameborder="0" scrolling="auto"></iframe> */}
     </App>
     </div>
   </Router>
